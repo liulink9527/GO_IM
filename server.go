fa6719db641718c07073b76d2c1adfbceb236347
liuwenqi <liulink666@163.com>
2025-07-22 14:08:37 +0800

    version 2 用户上线功能

--- server.go
+++ server.go
@@ -3,29 +3,63 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
 	IP   string
 	Port int
+	// 在线用户列表
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+	// 消息广播的channel
+	Message chan string
 }
 
 func NewServer(ip string, port int) *Server {
 	server := &Server{
-		IP:   ip,
-		Port: port,
+		IP:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
 	}
 	return server
 }
 
+// ListenMessager 监听消息 当Server广播管道中有消息时 发送给所有用户的管道
+func (s *Server) ListenMessager() {
+	for {
+		msg := <-s.Message
+		s.mapLock.Lock()
+		for _, user := range s.OnlineMap {
+			user.C <- msg
+		}
+		s.mapLock.Unlock()
+	}
+}
+
+func (s *Server) BroadCast(user *User, msg string) {
+	sendMessage := "[" + user.Addr + "]" + user.Name + ":" + msg
+	s.Message <- sendMessage
+}
+
 func (s *Server) Handler(conn net.Conn) {
 	defer conn.Close()
-	fmt.Println("连接建立成功 来自:", conn.RemoteAddr())
+
+	user := NewUser(conn)
+	// 记录上线用户
+	s.mapLock.Lock()
+	s.OnlineMap[user.Name] = user
+	s.mapLock.Unlock()
+	// 用户上线广播
+	s.BroadCast(user, "已上线")
+	// 当前handler阻塞
+	select {}
 }
 
 func (s *Server) Start() {
 	fmt.Println("服务器开始监听.............")
-	// socket listen
+	// 监听socket
 	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", s.IP, s.Port))
 	if err != nil {
 		fmt.Println("net.Listen err:", err)
@@ -34,16 +68,18 @@ func (s *Server) Start() {
 	// close listen socket
 	defer listener.Close()
 
+	//启动监听广播消息的协程
+	go s.ListenMessager()
+
 	for {
-		// accept
+		// 监听客户端连接
 		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Println("listener.accept err:", err)
 			continue
 		}
 
-		// do handler
+		// 处理连接
 		go s.Handler(conn)
 	}
-
 }
---
+++ user.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+type User struct {
+	Name string
+	Addr string
+	C    chan string
+	conn net.Conn
+}
+
+// NewUser 创建一个用户
+func NewUser(conn net.Conn) *User {
+	userAddr := conn.RemoteAddr().String()
+	user := &User{
+		Name: userAddr,
+		Addr: userAddr,
+		C:    make(chan string),
+		conn: conn,
+	}
+
+	// 启动监听
+	go user.ListenMessage()
+
+	return user
+}
+
+// ListenMessage 监听消息
+func (u *User) ListenMessage() {
+	for {
+		msg := <-u.C
+
+		_, err := u.conn.Write([]byte(msg + "\n"))
+		if err != nil {
+			fmt.Println("conn.Write err:", err)
+		}
+	}
+}
